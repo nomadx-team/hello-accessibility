1f628c4d8f5ebc551b980651460be9b61da6cae1
natemoo-re <ntmoore@knox.edu>
2018-05-28 00:21:38 -0600

    Add outline

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,12 @@
 console.log('Hello world!');
+
+document.getElementById('animate-numeronym').onclick = () => {
+    const h2 = document.querySelector('#a11y h2');
+    if (!h2.classList.contains('animated')) {
+        h2.classList.add('animating');
+        setTimeout(() => {
+            h2.classList.remove('animating');
+            h2.classList.add('animated');
+        }, 250);
+    }
+}
